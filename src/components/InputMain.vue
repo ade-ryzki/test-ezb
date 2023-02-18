efb660d80d48e27a67b05572da019aba49729bd4
ade-ryzki <ade.ryzki@gmail.com>
2023-02-19 04:03:53 +0700

    message same people done

--- src/components/InputMain.vue
+++ src/components/InputMain.vue
@@ -3,38 +3,38 @@
     <form @submit.prevent="submitMain" class="border w-1/2 mx-auto p-3 rounded shadow">
         <div class="mb-3">
             <label class="block my-1">Nama</label>
-            <input type="text" class="border border-red-300 px-2 py-1 rounded shadow w-full" v-model="name">
+            <input type="text" :class="[isName ? 'border-red-300' : '']" class="border px-2 py-1 rounded shadow w-full" v-model="name">
             <warning-message v-if="isName">{{NameMessage}}</warning-message>
         </div>
         <div class="mb-3">
             <label class="block my-1">Tujuan</label>
-            <select class="border border-red-300 px-2 py-2 rounded shadow w-full" v-model="tujuan">
+            <select :class="[isTujuan ? 'border-red-300' : '']" class="px-2 py-2 rounded shadow w-full" v-model="tujuan">
                 <option value="Bali">Bali</option>
                 <option value="Bromo">Bromo</option>
-                <option value="Maladewa">PLATINUM TOUR TRAVEL</option>
-                <option value="Bintan">GOLD TOUR TRAVEL</option>
-                <option value="Yogyakarta">SILVER TOUR TRAVEL</option>
+                <option value="Maladewa">Maladewa</option>
+                <option value="Bintan">Bintan</option>
+                <option value="Yogyakarta">Yogyakarta</option>
             </select>
             <warning-message v-if="isTujuan">{{TujuanMessage}}</warning-message>
         </div>
         <div class="mb-3">
             <label class="block my-1">Paket</label>
-            <input type="text" class="border border-red-300 px-2 py-1 rounded shadow w-full" v-model="paket">
+            <input type="text" :class="[isPaket ? 'border-red-300' : '']" class="px-2 py-1 rounded shadow w-full" v-model="paket">
             <warning-message v-if="isPaket">{{PaketMessage}}</warning-message>
         </div>
         <div class="mb-3">
             <label class="block my-1">Tanggal</label>
-            <input type="text" class="border border-red-300 px-2 py-1 rounded shadow w-full" v-model="tanggal">
+            <input type="text" :class="[isTanggal ? 'border-red-300' : '']" class="px-2 py-1 rounded shadow w-full" v-model="tanggal">
             <warning-message v-if="isTanggal">{{TanggalMessage}}</warning-message>
         </div>
         <div class="mb-3">
             <label class="block my-1">Harga</label>
-            <input type="text" class="border border-red-300 px-2 py-1 rounded shadow w-full" v-model="harga">
+            <input type="text" :class="[isHarga ? 'border-red-300' : '']" class="px-2 py-1 rounded shadow w-full" v-model="harga">
             <warning-message v-if="isHarga">{{HargaMessage}}</warning-message>
         </div>
         <div class="mb-3">
             <label class="block my-1">Service</label>
-            <select class="border border-red-300 px-2 py-2 rounded shadow w-full" v-model="service">
+            <select :class="[isService ? 'border-red-300' : '']" class="px-2 py-2 rounded shadow w-full" v-model="service">
                 <option value="VVIP TOUR TRAVEL">VVIP TOUR TRAVEL</option>
                 <option value="VIP TOUR TRAVEL">VIP TOUR TRAVEL</option>
                 <option value="PLATINUM TOUR TRAVEL">PLATINUM TOUR TRAVEL</option>
@@ -45,16 +45,29 @@
         </div>
         <div class="mb-3">
             <label class="block my-1">Total</label>
-            <input type="text" class="border border-red-300 px-2 py-1 rounded shadow w-full" v-model="total">
+            <input type="text" :class="[isTotal ? 'border-red-300' : '']" class="px-2 py-1 rounded shadow w-full" v-model="total">
             <warning-message v-if="isTotal">{{TotalMessage}}</warning-message>
         </div>
         <blue-button>Save</blue-button>
     </form>
 
+    <telport to="body">
+        <confirm-dialog v-if="isConfirmDialog">
+            <template v-slot:title>Informasi</template>
+            <template v-slot:message>Nama {{ name }} sudah ada!</template>
+            <template v-slot:button>
+                <blue-button @click="closeConfirmDialog">Close</blue-button>
+            </template>
+        </confirm-dialog>
+    </telport>
+
 </template>
 
 <script>
+
+
 export default{
+    inject: ['mains'],
     data(){
         return{
             name: '',
@@ -64,6 +77,7 @@ export default{
             harga: '',
             service: '',
             total: '',
+            isConfirmDialog: false,
             isName: false,
             isTujuan: false,
             isPaket: false,
@@ -82,35 +96,58 @@ export default{
         }
     },
     methods: {
-        submitMain(){
+        submitMain() {
             if (!this.name.trim()){
                 this.isName = true
                 this.NameMessage = "Name harus diisi."
+            } else {
+                this.isName = false
             }
             if (!this.tujuan.trim()){
                 this.isTujuan = true
                 this.TujuanMessage = "Tujuan harus diisi."
+            } else {
+                this.isTujuan = false
             }
             if (!this.paket.trim()){
                 this.isPaket = true
                 this.PaketMessage = "Paket harus diisi."
+            } else {
+                this.isPaket = false
             }
             if (!this.tanggal.trim()){
                 this.isTanggal = true
                 this.TanggalMessage = "Tanggal harus diisi."
+            } else {
+                this.isTanggal = false
             }
             if (!this.harga.trim()){
                 this.isHarga = true
                 this.HargaMessage = "Harga harus diisi."
+            } else {
+                this.isHarga = false
             }
             if (!this.service.trim()){
                 this.isService = true
                 this.ServiceMessage = "Service harus diisi."
+            } else {
+                this.isService = false
             }
             if (!this.total.trim()){
                 this.isTotal = true
                 this.TotalMessage = "Total harus diisi."
+            }else {
+                this.isTotal = false
             }
+            if(!this.isName && !this.isTujuan && !this.isPaket && !this.isTanggal && !this.isHarga && !this.isService && !this.isTotal) {
+                var searchingMain = this.mains.find(main => main.name === this.name)
+                if (searchingMain) {
+                    this.isConfirmDialog = true
+                }
+            }
+        },
+        closeConfirmDialog() {
+            this.isConfirmDialog = false
         }
     }
 }
--- src/components/UI/ConfirmDialog.vue
+++ src/components/UI/ConfirmDialog.vue
@@ -1,5 +1,5 @@
 <template>
-        <div id="dialog" class="absolute bg-white border w-fit shadow rounded px-4 py-3 top-1/2 left-1/2">
+        <div id="dialog" class="absolute bg-white border w-fit shadow rounded px-4 py-3 top-1/2 left-1/2 -translate-1/2 -translate-y1/2">
         <h3 class="font-bold text-lg my-2"><slot name="title"></slot></h3>
         <div id="dialog-message" class="my-2"><slot name="message"></slot></div>
         <div id="dialog-button" class="my-2"><slot name="button"></slot></div>
